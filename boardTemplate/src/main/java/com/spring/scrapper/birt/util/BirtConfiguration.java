be6277aefb1684b99d368d9dd6d159523917539e
soongujung <playmymind@daum.net>
2018-07-11 23:46:08 +0900

    BIRT VIEWER 기능 초기 구성 (미완)

--- boardTemplate/src/main/java/com/spring/scrapper/birt/util/BirtConfiguration.java
+++ boardTemplate/src/main/java/com/spring/scrapper/birt/util/BirtConfiguration.java
@@ -5,6 +5,7 @@ import java.util.logging.Level;
 import org.eclipse.birt.report.engine.api.EngineConfig;
 import org.eclipse.birt.report.engine.api.HTMLActionHandler;
 import org.eclipse.birt.report.engine.api.HTMLEmitterConfig;
+import org.eclipse.birt.report.engine.api.HTMLRenderOption;
 import org.eclipse.birt.report.engine.api.HTMLServerImageHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,47 +14,47 @@ import org.slf4j.LoggerFactory;
 public class BirtConfiguration {
 	private final Logger LOGGER = LoggerFactory.getLogger(BirtConfiguration.class);
 	
-//	private String birtRuntimeLocation;
-//	private String logLocation;
-//	private Level logLevel;
-//	private EngineConfig engineConfig;
-//	
-//	public BirtConfiguration(){
-//		logLevel = Level.WARNING;
-//	}
-//	
-//	public void setBirtRuntimeLocation(String birtRuntimeLocation) {
-//	    this.birtRuntimeLocation = birtRuntimeLocation;
-//	}
-//	
-//	public EngineConfig getEngineConfig() {
-//	    if (engineConfig == null) {
-//	      if (LOGGER.isDebugEnabled()) {
-//	        LOGGER.debug("Creating new instance of EngineConfig");
-//	      }
-//
-//	      engineConfig = new EngineConfig();
-//	      engineConfig.setEngineHome(birtRuntimeLocation);
-//	      engineConfig.setLogConfig(logLocation, logLevel);
-//
-//	      HTMLEmitterConfig htmlConfig = new HTMLEmitterConfig();
-//	      htmlConfig.setActionHandler(new HTMLActionHandler());
-//	      
-//	      // This allows images to be referenced by url
-//	      htmlConfig.setImageHandler(new HTMLServerImageHandler());
-//
-//	      engineConfig.getEmitterConfigs().put("html", htmlConfig);
-//	    }
-//
-//	    return engineConfig;
-//	}
-//
-//	public String getLogLocation() {
-//		return logLocation;
-//	}
-//
-//	public void setLogLocation(String logLocation) {
-//		this.logLocation = logLocation;
-//	}
+	private String birtRuntimeLocation;
+	private String logLocation;
+	private Level logLevel;
+	private EngineConfig engineConfig;
+	
+	public BirtConfiguration(){
+		logLevel = Level.WARNING;
+	}
+	
+	public void setBirtRuntimeLocation(String birtRuntimeLocation) {
+	    this.birtRuntimeLocation = birtRuntimeLocation;
+	}
+	
+	public EngineConfig getEngineConfig() {
+	    if (engineConfig == null) {
+	      if (LOGGER.isDebugEnabled()) {
+	        LOGGER.debug("Creating new instance of EngineConfig");
+	      }
+
+	      engineConfig = new EngineConfig();
+	      engineConfig.setEngineHome(birtRuntimeLocation);
+	      engineConfig.setLogConfig(logLocation, logLevel);
+
+	      HTMLRenderOption htmlConfig = new HTMLRenderOption();
+	      htmlConfig.setActionHandler(new HTMLActionHandler());
+	      
+	      // This allows images to be referenced by url
+	      htmlConfig.setImageHandler(new HTMLServerImageHandler());
+
+	      engineConfig.getEmitterConfigs().put("html", htmlConfig);
+	    }
+
+	    return engineConfig;
+	}
+
+	public String getLogLocation() {
+		return logLocation;
+	}
+
+	public void setLogLocation(String logLocation) {
+		this.logLocation = logLocation;
+	}
 	
 }
